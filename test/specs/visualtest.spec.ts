32e2ae653b98127289d8e4715584e4a4bd3fb969
anhduong1611 <hothianhduong16112002@gmail.com>
2024-10-31 16:50:37 +0700

    Ok 2

--- test/specs/visualtest.spec.ts
+++ test/specs/visualtest.spec.ts
@@ -38,4 +38,10 @@ describe('Visual', () => {
         const result = await browser.checkElement(logo, 'Chay');
         expect(result).toBeLessThanOrEqual(0.05);
     });
+    it.only('Check checkTabbablePage 1234', async () => {
+        browser.url('https://www.webtaphoa.vn/account/register');
+        await browser.pause(2000);
+        await browser.checkTabbablePage('save-tabbable', {
+        })
+    });
 })
